45a67a3987f214232234d2b38adacc5bfd598eed
Sushy-trash <daniel.pearce.exe@gmail.com>
2026-05-24 12:54:48 -0400

    commit generacion de tabla clase genero y atributos

---
+++ src/main/java/com/example/syntax_error_studios/model/Consola.java
@@ -0,0 +1,5 @@
+package com.example.syntax_error_studios.model;
+
+public class Consola {
+
+}
---
+++ src/main/java/com/example/syntax_error_studios/model/Genero.java
@@ -0,0 +1,30 @@
+package com.example.syntax_error_studios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name= "generos")
+public class Genero {
+
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Notblank(message="El nombre del genero es obligatorio")
+    @Size(max=100,message="El nombre del genero no puede tener mas de 100 carateres")
+    @collumn(nullable=false,length=100)
+    private String nombre;
+}
