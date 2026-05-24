02d66f9b62558c7fa0476f173bd554feb0bc0962
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 19:49:59 -0400

    Actualización base VideojuegoService
    Se actualizan los métodos base de VideojuegosService

--- src/main/java/com/example/syntax_error_studios/service/VideojuegoService.java
+++ src/main/java/com/example/syntax_error_studios/service/VideojuegoService.java
@@ -1,5 +1,40 @@
 package com.example.syntax_error_studios.service;
 
-public class VideojuegoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.example.syntax_error_studios.model.Videojuego;
+import com.example.syntax_error_studios.repository.VideojuegoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VideojuegoService {
+    
+    private final VideojuegoRepository videojuegoRepository;
+
+    //Obtener todos los videojuegos
+    public List<Videojuego> obtenerTodos(){
+        return videojuegoRepository.findAll();
+    }
+
+    //Obtener videojuego por ID
+    public Optional<Videojuego> obtenerPorId(Long id){
+        return videojuegoRepository.findById(id);
+    }
+
+    //Guardar videojuego
+    public Videojuego guardar(Videojuego videojuego){
+        return videojuegoRepository.save(videojuego);
+    }
+
+    //Eliminar videojuego
+    public void eliminar(Long id){
+        videojuegoRepository.deleteById(id);
+    }
+
+    
 }
