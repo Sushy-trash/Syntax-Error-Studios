a280f92937046bc1e75f589a08fe07e43600e864
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 17:10:30 -0400

    Creación Microservicio buscar por nombre
    Se crea el microservicio usando native query para la búsqueda por nombre de videojuego

--- src/main/java/com/example/syntax_error_studios/repository/VideojuegoRepository.java
+++ src/main/java/com/example/syntax_error_studios/repository/VideojuegoRepository.java
@@ -1,9 +1,21 @@
 package com.example.syntax_error_studios.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.syntax_error_studios.model.Videojuego;
 
 public interface VideojuegoRepository extends JpaRepository<Videojuego, Long> {
+
+    //Buscar por nombre de videojuego
+    @Query(
+        value = "SELECT * FROM videojuegos WHERE nombre_videojuego LIKE CONCAT ('%', :texto, '%')",
+        nativeQuery = true
+    )
+    List <Videojuego> buscarPorNombre(@Param("texto")String texto);
+    
 
 }
