4c5c05edd8e4251514636deb740c4b0d4e15f55d
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 19:45:29 -0400

    Actualización de ModalidadService
    Se actualizan los métodos para la clase Modalidad Service

--- src/main/java/com/example/syntax_error_studios/service/ModalidadService.java
+++ src/main/java/com/example/syntax_error_studios/service/ModalidadService.java
@@ -1,5 +1,39 @@
 package com.example.syntax_error_studios.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.syntax_error_studios.model.Modalidad;
+import com.example.syntax_error_studios.repository.ModalidadRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ModalidadService {
+    
+    private final ModalidadRepository modalidadRepository;
+
+    //Obtener todas las modalidades
+    public List<Modalidad> obtenerTodos() {
+        return modalidadRepository.findAll();
+    }
+
+    // Obtener modalidad por ID
+    public Optional<Modalidad> obtenerPorId(Long id) {
+        return modalidadRepository.findById(id);
+    }
+
+    // Guardar modalidad
+    public Modalidad guardar(Modalidad modalidad) {
+        return modalidadRepository.save(modalidad);
+    }
+
+    // Eliminar modalidad
+    public void eliminar(Long id) {
+        modalidadRepository.deleteById(id);
+    }
 
 }
