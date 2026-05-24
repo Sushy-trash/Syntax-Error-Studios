317b0b5968766df26cd5d1f8974c05f67d3c2b02
Paula Muñoz González <paula.munozg@duocuc.cl>
2026-05-24 13:47:05 -0400

    Actualización clase modalidad
    Se actualiza la clase "modalidad" añadiéndole los atributos junto con las validaciones y atajos de persistencia.

--- src/main/java/com/example/syntax_error_studios/model/Modalidad.java
+++ src/main/java/com/example/syntax_error_studios/model/Modalidad.java
@@ -1,5 +1,23 @@
 package com.example.syntax_error_studios.model;
 
-public class Modalidad {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "modalidades")
+public class Modalidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message="El nombre de la modalidad es obligatorio")
+    @Size(max=100,message="El nombre de la modalidad no puede tener mas de 100 carateres")
+    @Column(nullable=false,length=100)
+    private String nombreModalidad;  
 }
